d258af0cdffb648d5e7833915298e81bae92dad3
anjiann <andy.jian09@gmail.com>
2025-05-26 11:41:44 -0400

    Update various dev configs

--- .config/nvim/lua/plugins/avante.lua
+++ .config/nvim/lua/plugins/avante.lua
@@ -3,9 +3,7 @@ return {
   event = "VeryLazy",
   version = false, -- Never set this value to "*"! Never!
   opts = {
-    -- add any opts here
-    -- for example
-    provider = "openai",
+    provider = "gemini",
     openai = {
       endpoint = "https://api.openai.com/v1",
       model = "gpt-4o", -- your desired model (or use gpt-4o, etc.)
@@ -14,6 +12,19 @@ return {
       max_completion_tokens = 8192, -- Increase this to include reasoning tokens (for reasoning models)
       --reasoning_effort = "medium", -- low|medium|high, only used for reasoning models
     },
+    gemini = {
+      model = "gemini-2.5-pro-exp-03-25",
+      timeout = 30000,
+      temperature = 0,
+      max_tokens = 8192,
+      api_key_name = os.getenv("GEMINI_API_KEY"),
+      --reasoning_effort = "medium", -- low|medium|high, only used for reasoning models
+    },
+    -- file_selector = {
+    --     provider = "snacks",
+    --     -- Options override for custom providers
+    --     provider_opts = {},
+    -- },
   },
   -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
   build = "make",
--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,23 +1,45 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  opts = {
-    ensure_installed = {
-      "bash",
-      "html",
-      "javascript",
-      "json",
-      "lua",
-      "markdown",
-      "markdown_inline",
-      "python",
-      "query",
-      "regex",
-      "tsx",
-      "typescript",
-      "vim",
-      "yaml",
-      "latex",
-      "tsx",
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "bash",
+        "html",
+        "javascript",
+        "json",
+        "lua",
+        "markdown",
+        "markdown_inline",
+        "python",
+        "query",
+        "regex",
+        "tsx",
+        "typescript",
+        "vim",
+        "yaml",
+        "latex",
+        "tsx",
+      },
     },
+  },
+
+  {
+    "nvim-treesitter/nvim-treesitter-context",
+    event = "VeryLazy", -- or "BufReadPre" for earlier loading
+    config = function()
+      require("treesitter-context").setup({
+        -- Optional configuration, see below for options
+        enable = true, -- Enable this plugin (Can be enabled/disabled later via commands)
+        max_lines = 0, -- How many lines the window should span. 0 = infinite
+        min_window_height = 0, -- Minimum editor window height to enable context
+        line_numbers = true, -- Show line numbers
+        multiline_threshold = 20, -- Maximum number of lines to show for a single context
+        trim_scope = "outer", -- Which context lines to discard if `max_lines` is exceeded. Choices: 'inner', 'outer'
+        mode = "cursor", -- Line used to calculate context. Choices: 'cursor', 'topline'
+        separator = nil, -- Separator between context and content. e.g. "-". Set to nil to disable
+        zindex = 20, -- The Z-index of the context window
+        on_attach = nil, -- (fun(buf: integer): boolean) return false to disable attaching
+      })
+    end,
   },
 }
